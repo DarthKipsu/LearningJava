4e0957d9e7228078839e7348f0c91e58e064277b
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-22 11:43:02 +0300

    solve towers of hanoi with N pegs

--- src/recursive/TowersOfHanoi.java
+++ src/recursive/TowersOfHanoi.java
@@ -3,79 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package recursive;
 
-import java.util.*;
+package recursive;
 
 /**
  * This program will solve the towers of Hanoi with N amount of pegs as a
  * starting value.
  */
 public class TowersOfHanoi {
-
-    public static void createPegs(int a) {
-        List<Stack<Integer>> pegs = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            pegs.add(new Stack<Integer>());
-        }
-        for (int i = 0; i < a; i++) {
-            pegs.get(0).push(a - i);
-        }
-        System.out.println(a + " pegs: " + pegs);
-        
-        makeMove(pegs, a);
-        System.out.println("");
-        System.out.println(a + " pegs moved: " + pegs);
+    
+    public static void solvePegs(int nPegs) {
+        makeAMove(nPegs, 1, 3);
         System.out.println("");
     }
-
-    public static void makeMove(List<Stack<Integer>> pegs, int a) {
-        if (pegs.get(2).size() == a) {
-            return;
-        }
-        int move;
-        if (pegs.get(1).empty() || pegs.get(2).empty()) {
-            if (pegs.get(0).size() % 2 == 0) {
-                move = pegs.get(0).pop();
-                pegs.get(1).push(move);
-                System.out.print(move + "->" + 2 + " ");
-                makeMove(pegs, a);
-            } else {
-                move = pegs.get(0).pop();
-                pegs.get(2).push(move);
-                System.out.print(move + "->" + 3 + " ");
-                makeMove(pegs, a);
-            }
+    
+    public static void makeAMove(int nPegs, int start, int goal) {
+        if (nPegs == 1) {
+            System.out.print(nPegs + "->" + goal + " ");
         } else {
-            int bigPeg = pegs.get(1).lastElement();
-            int list = 1;
-            if (pegs.get(2).lastElement()>bigPeg) {
-                bigPeg = pegs.get(2).lastElement();
-                list = 2;
-            }
-            int movePeg = bigPeg - 1;
-            int moveList = 2;
-            if (pegs.get(1).contains(movePeg)) {
-                moveList = 1;
-            } else if (pegs.get(0).contains(movePeg)) {
-                moveList = 0;
-            }
-            
-            if (pegs.get(list).lastElement()==bigPeg) {
-                if (pegs.get(moveList).lastElement()==movePeg) {
-                    move = pegs.get(moveList).pop();
-                    pegs.get(list).push(move);
-                    System.out.print(move + "->" + (list + 1) + " ");
-                    makeMove(pegs, a);
-                } else {
-                    if (pegs.get(moveList).size() % 2 == 0) {
-                        move = pegs.get(moveList).pop();
-                        pegs.get(0).push(move);
-                        System.out.print(move + "->" + 1 + " ");
-                        makeMove(pegs, a);
-                    }
-                }
-            }
+            int temp = 6 - start - goal;
+            makeAMove(nPegs-1, start, temp);
+            System.out.print(nPegs + "->" + goal + " ");
+            makeAMove(nPegs-1, temp, goal);
         }
     }
 
@@ -83,9 +32,9 @@ public class TowersOfHanoi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        createPegs(3);
-        createPegs(4);
-        createPegs(5);
+        solvePegs(3);
+        solvePegs(4);
+        solvePegs(5);
     }
-
+    
 }
