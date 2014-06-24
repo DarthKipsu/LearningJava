992515615c8dcc15eb2f494e70995dacc90e9adc
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-24 21:36:31 +0300

    add binary search for suffixes

--- src/graphs/SuffixArray.java
+++ src/graphs/SuffixArray.java
@@ -24,19 +24,19 @@ public class SuffixArray {
         String string1 = "abbaababbababbab"; // example 3
         String string2 = "aacatcgatagctagaacat"; // actual assignment
         
-        printResults(string1);
-        printResults(string2);
+        String search1 = "ababbaba";
+        String search2 = "cga";
+        
+        printResults(string1, search1);
+        printResults(string2, search2);
     }
     
-    public static void printResults(String string) {
+    public static void printResults(String string, String search) {
         Suffix[] suffixArray = createSuffixArray(string);
         
-        System.out.println(string + ", length: " + string.length());
-        for (int i=0; i<string.length(); i++) {
-            System.out.print(suffixArray[i].get() + " ");
-            System.out.print(suffixArray[i].index() + " ");
-            System.out.println(suffixArray[i].sIndex());
-        }
+        System.out.println("t = " + string + ", p = " + search);
+        printSuffixArray(suffixArray);
+        printSearchWord(suffixArray, search);
         System.out.println("");
     }
     
@@ -60,6 +60,47 @@ public class SuffixArray {
                     original[i] = original[j];
                     original[j] = temp;
                 }
+            }
+        }
+    }
+    
+    public static void printSuffixArray(Suffix[] suffixArray) {
+        System.out.println("Suffix array:");
+        for (int i=0; i<suffixArray.length; i++) {
+            System.out.print("   " + suffixArray[i].get() + " ");
+            System.out.print(suffixArray[i].index() + " ");
+            System.out.println(suffixArray[i].sIndex());
+        }
+    }
+    
+    public static void printSearchWord(Suffix[] suffixArray, String search) {
+        int begin = 1;
+        int end = suffixArray.length;
+        System.out.println("1. Starting values, begin = " + begin + ", end = " + end + ".");
+        binarySearch(suffixArray, search, begin, end, 2);
+    }
+    
+    public static void binarySearch(Suffix[] suffixArray, String search,
+                                    int begin, int end, int i) {
+        if (begin > end) {
+            System.out.println("Search ended, character p could not be found.");
+        } else {
+            int middle = (int)Math.ceil((begin + end) / 2.0);
+            String comparison = suffixArray[middle-1].get();
+            if (search.length() < comparison.length()) {
+                comparison = comparison.substring(0, search.length());
+            }
+            System.out.println(i + ". Middle = (" + begin + "+" + end + ")/2 = " + middle + ",");
+            if (search.compareTo(comparison) < 0) {
+                System.out.println("   p < t[S[" + middle + "].." + end + "], end = middle-1 = " + (middle-1) + ".");
+                end = middle - 1;
+                binarySearch(suffixArray, search, begin, end, i+1);
+            } else if (search.compareTo(comparison) > 0) {
+                System.out.println("   p > t[S[" + middle + "].." + end + "], begin = middle+1 = " + (middle+1) + ".");
+                begin = middle + 1;
+                binarySearch(suffixArray, search, begin, end, i+1);
+            } else {
+                System.out.println("   p == t[S[" + middle + "].." + end + "], Search ended at " + middle + ".");
             }
         }
     }
