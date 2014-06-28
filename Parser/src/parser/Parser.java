b14c13170c7452d3fff38b8512f85465b536e9d8
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-28 19:02:06 +0300

    add parser to create nodes

--- Parser/src/parser/Parser.java
+++ Parser/src/parser/Parser.java
@@ -11,12 +11,38 @@ package parser;
  * @author kipsu
  */
 public class Parser {
+    
+    public static Node createNode(String input) {
+        Node root = new Node();
+        Node current = root;
+        String tempNum = "";
+        for (int i=0; i<input.length(); i++) {
+            switch(input.charAt(i)) {
+                case '+':
+                    Node sum = new Sum();
+                    current.setRight(sum);
+                    current = sum;
+                    Number number = new Number(new Double(tempNum));
+                    current.setLeft(number);
+                    tempNum = "";
+                    break;
+                default:
+                    tempNum += input.charAt(i);
+            }
+        }
+        Number number = new Number(new Double(tempNum));
+        current.setRight(number);
+        
+        return root.getRight();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String input1 = "2+2";
+        
+        Node node1 = createNode(input1);
     }
     
 }
